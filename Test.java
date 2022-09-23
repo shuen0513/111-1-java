d58778344701dc0eb2925fd0fe9066b8794fd24a
shuen0513 <114204120+shuen0513@users.noreply.github.com>
2022-09-23 11:41:41 +0800

    2022.09.23 java-2

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 public class Test{
     public static void main(String[] args) {
             System.out.println(args[0]);
-  	    System.out.println(args[1]);
+  	    System.out.println("Second Version");
     }
 } 	
