a1b37f6ac3f25fe76e7ea47a8b573e1e9940a482
shuen0513 <114204120+shuen0513@users.noreply.github.com>
2022-10-07 11:40:35 +0800

    2022.10.07 java Test7

---
+++ Test7.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Test7{
+    public static void main(String[] args) {
+        int grade=0;
+        String ari="";
+
+        while (grade <=9) {
+            System.out.printf(ari);
+            System.out.printf("%d%n",grade);
+            grade = grade+1;
+            ari = ari+" ";  
+        }
+    }
+}
