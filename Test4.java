0d4a876bac1d6974a9331b14ad1efeb9de9daf00
shuen0513 <114204120+shuen0513@users.noreply.github.com>
2022-10-07 11:47:28 +0800

    Test4

--- Test4.java
+++ Test4.java
@@ -2,31 +2,19 @@ import java.util.Scanner;
 
 public class Test4{
     public static void main(String[] args) {
-        int grade;
-		
+        int num1;
+
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter first integer");
-		grade = input.nextInt();
-		
-        switch(grade/10){
-            case 9 :
-            case 10:
-                printStr("A");
-                break;
-            case 8:
-                printStr("B");
-                break;
-             case 7:
-                printStr("C");
-                break;
-             case 6:
-                printStr("D");
-             default:
-                printStr("E");
-                break;
-        }
-    }
-    public static void printStr(String str) {
-        System.out.println(str);
-    }
+		num1 = input.nextInt();
+
+		if (num1 <= 60) { 
+			if (num1 <= 70) {
+			    System.out.println("<=70");
+			}
+		}			
+            else { 
+			System.out.println(">70");
+	    }    
+	}
 }
