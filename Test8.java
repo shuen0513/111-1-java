b0c7e9eb6aad1398d3a364b7ac1207b4f3ceae10
shuen0513 <114204120+shuen0513@users.noreply.github.com>
2022-10-14 12:32:53 +0800

    Test8.java 2022.10.14

--- Test8.java
+++ Test8.java
@@ -2,17 +2,19 @@ import java.util.Scanner;
 
 public class Test8{
     public static void main(String[] args) {
-        int row = 0 ;
+    int row = 0 ;
 		
 		while(row < 5 ) {
-			int star = 0 
-			while(star<=row){
+		  int star = 0 ;
+		  while(star<=row){
 				System.out.print("*");
 				star=star+1;
-			}
-			
-
-
-        
+      
+		  }
+        System.out.println("");			
+        row +=1;
+      
+        }
     }
 }
+    
