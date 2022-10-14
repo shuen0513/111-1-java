9d8d8ad198c01a5a90dbdfd915d26c51e2d136c6
shuen0513 <114204120+shuen0513@users.noreply.github.com>
2022-10-14 11:52:03 +0800

    Add files via upload

---
+++ Test8.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Test8{
+    public static void main(String[] args) {
+        int row = 0 ;
+		
+		while(row < 5 ) {
+			int star = 0 
+			while(star<=row){
+				System.out.print("*");
+				star=star+1;
+			}
+			
+
+
+        
+    }
+}
