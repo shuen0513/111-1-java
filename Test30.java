5ac4133b191481fd49526a30f85ccdf4620f4f77
shuen0513 <114204120+shuen0513@users.noreply.github.com>
2022-12-23 11:07:30 +0800

    2022.12.23

--- Test30.java
+++ Test30.java
@@ -1,18 +1,42 @@
+import java.util.Scanner;
+
 public class Test30 {
     public static void main(String[] args) {
-        int temp=0;
-        int[] arr = { 7, 5, 4, 9, 8, 1, 2 };
-        float alls = 0;
-        for(int i=0;i<arr.length*arr.length;i++){
+        int[] myArr = { 7, 5, 4, 9, 8, 1, 2 };
+        int[] flag= new int[myArr.length];
+        int flag_count=1;
 
-        }
-        System.out.printf("MAX:%d\n", arr[arr.length - 1]);
-        System.out.printf("MIN:%d\n", arr[0]);
-        for (int i = 0; i < arr.length; i++) {
-            alls += arr[i];
-        }
-        System.out.printf("AVR:%f\n", alls / arr.length);
-        System.out.printf("MID:%d\n", arr[(arr.length - 1) / 2]);
+        int max=0;
+        int min=10;
+        int sum=0;
+        float avg;
 
+        for(int i=0;i<myArr.length;i++){
+            if(myArr[i]>max)
+                max=myArr[i];
+            if(myArr[i]<min)
+                min=myArr[i];
+            sum+=myArr[i];
+        }
+        avg=(float)sum/myArr.length;
+
+        int med = 0;
+        for(int i=0;i<4;i++){
+            int flag_min=1000;
+            int flag_min_index=0;
+            for(int j = 0;j<myArr.length;j++){
+                if(myArr[j] < flag_min && flag[j]==0 ){
+                    flag_min=myArr[j];
+                    flag_min_index=j;
+                }
+            }
+            flag[flag_min_index] = flag_count;
+            flag_count++ ;
+            med=myArr[flag_min_index];
+        }		
+        System.out.println("max:" + max);
+        System.out.println("min:" + min);
+        System.out.println("avg:" + avg);
+        System.out.println("med:" + med);
     }
 }
